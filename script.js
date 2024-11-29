ba2bcbd5d23f6601cc8d7f595f538e626cab469f
lise-charlotte <lisecharlottekoder@gmail.com>
2024-11-29 14:13:50 +0100

    filled out, mostly complete

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // Reference to HTML elements
-const  = document.querySelector("#");
-const  = document.querySelector("#");
-const  = document.querySelector("#");
+const inputField = document.querySelector("#inputField");
+const list = document.querySelector("#list");
+const addButton = document.querySelector("#addButton");
 
 // Function to add a new item to the list
 function addItem() {
-  const  = .value;
+  const item = inputField.value;
 
     // Create a new list item and give content
-    const  = document.createElement("");
-    .textContent = ;
+    const li = document.createElement("li");
+    li.textContent = item;
 
     // Create a remove button for the new list item
-    const  = document.createElement("");
-    .textContent = "Slett";
-    .classList.add("");
+    const removeButton = document.createElement("Button");
+    removeButton.textContent = "Slett";
+    removeButton.classList.add("remove-btn");
 
     // Add an event listener to the remove button
-    .addEventListener("click", function () {
+    removeButton.addEventListener("click", function () {
       removeItem(li)
     });
 
     // Append the remove button to the list item
-    li.appendChild();
+    li.appendChild(removeButton);
 
     // Append the list item to the list
-    list.appendChild();
+    list.appendChild(li);
 }
 
 // Function to remove an item from the list
